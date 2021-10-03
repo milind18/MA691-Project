87978485070b0f00add24aa37695472681b39184
coolgoel <coolgoel18@gmail.com>
2021-10-03 16:08:14 +0530

    basic setup for heroku

--- insurance_calculator_cobra/insurance_calculator_cobra/settings.py
+++ insurance_calculator_cobra/insurance_calculator_cobra/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')
 ]
 
 # Default primary key field type
