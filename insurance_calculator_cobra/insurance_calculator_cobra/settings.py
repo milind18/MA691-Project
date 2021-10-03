5d3dd50c9660c285cadbb4f12cd8e7559fcb2315
coolgoel <coolgoel18@gmail.com>
2021-10-03 23:37:35 +0530

    directory cleanup

--- insurance_calculator_cobra/insurance_calculator_cobra/settings.py
+++ insurance_calculator_cobra/insurance_calculator_cobra/settings.py
@@ -46,10 +46,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
